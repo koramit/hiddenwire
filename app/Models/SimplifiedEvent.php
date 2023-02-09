83c93998973f19a0cd719592d38ec58761d48d12
Koramit Pichanaharee <koramit.p@gmail.com>
2023-02-09 11:04:42 +0700

    add cast

--- app/Models/SimplifiedEvent.php
+++ app/Models/SimplifiedEvent.php
@@ -2,12 +2,22 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Carbon;
 
 class SimplifiedEvent extends Model
 {
     use HasFactory;
 
     protected $guarded = [];
+
+    protected function timestamp(): Attribute
+    {
+        return Attribute::make(
+            get: fn ($value) => Carbon::create($value),
+            set: fn ($value) => Carbon::createFromTimestamp($value/1000),
+        );
+    }
 }
